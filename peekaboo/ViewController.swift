3c7f971c30c5599765ad94368cd6955d308bd040
yan xia <yanxia@yans-mbp.corp.kabam.com>
2015-02-04 17:37:28 -0800

    add page control, but it doesn't work yet

--- peekaboo/ViewController.swift
+++ peekaboo/ViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myPageControl: UIPageControl!
+    
+    @IBOutlet weak var myColorView: UIView!
+    
+    let myColors = [
+        UIColor.blackColor(),
+        UIColor.grayColor(),
+        UIColor.redColor(),
+        UIColor.greenColor(),
+        UIColor.blueColor(),
+        UIColor.cyanColor(),
+        UIColor.yellowColor(),
+        UIColor.magentaColor(),
+        UIColor.orangeColor(),
+        UIColor.purpleColor()
+    ]
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configMyPageControl()
+        pageControlValueDidChange()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func configMyPageControl(){
+        myPageControl.numberOfPages = myColors.count
+        myPageControl.currentPage = 0
+        myPageControl.tintColor = UIColor.blueColor()
+        myPageControl.pageIndicatorTintColor = UIColor.greenColor()
+        myPageControl.currentPageIndicatorTintColor = UIColor.purpleColor()
+        myPageControl.addTarget(self, action: "pageControlValueDidChange", forControlEvents: .ValueChanged)
+    }
+    
+    func pageControlValueDidChange() {
+        NSLog("The page control changed its current page to \(myPageControl.currentPage).")
+        myColorView.backgroundColor = myColors[myPageControl.currentPage]
     }
 
 
